890fd538f0e4f99771a0de53e20f250dd5fcf306
Jose Jimenez-Olivas <josejimenezjr18@gmail.com>
2019-07-05 16:33:17 -0700

    system, tests, testruns done. In need for sub server endpint

--- lab_tester/route.py
+++ lab_tester/route.py
@@ -2,13 +2,17 @@ from flask import Blueprint
 from flask_restful import Api
 
 # Resources
-#from .resources.Hello import Hello 
-#from .resources import Testruns
+from .resources.systems import Systems
+from .resources.tests import Tests
 from .resources.testruns import Testruns
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-#api.add_resource(Hello, '/Hello')
+Systems.add_all_resources(api, '/systems')
+Tests.add_all_resources(api, '/tests')
 Testruns.add_all_resources(api, '/testruns')
+
+
+
