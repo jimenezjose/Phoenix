195d99d418a6d59635e5ac44da30e821f40c492b
Jose Jimenez-Olivas <josejimenezjr18@gmail.com>
2019-09-11 20:17:10 -0700

    finished to present with half implemented logs

--- api/resources/testsruns/id/__init__.py
+++ api/resources/testsruns/id/__init__.py
@@ -1,20 +1,21 @@
-from api.db import get_db, get_table, validate
-# local resources
 from .log import TestsrunsLog
-from .status import TestsrunsStatus
+from .update import TestsrunsUpdate
+from api.db import (
+    get_table, 
+    validate)
 
 from flask_restful import Resource
 
 class TestsrunsID(Resource):
   """testsruns/<int:id> resource to get all info about a testsrun."""
 
-  def get(self, id):
+  def get(self, tests_runs_id):
     """GET request for (hostname, tests name, start time, end time, notes, config, scratch)
     on testsrun with url passed 'id'
 
     Args:
-        id: integer identification of a specific testsrun in interest passed through url.  
-
+        tests_runs_id : Integer identification of a specific testsrun in interest passed 
+                        through url.
     Returns:
         Success:
             Status Code: 200 OK
@@ -23,8 +24,8 @@ class TestsrunsID(Resource):
             Status Code: 404 Not Found
                 * testsruns row 'id' does not exists in database.
     """
-    validate(tests_runs_id=id, http_error_code=404)
-    tests_run = get_table('tests_runs', tests_runs_id=id)
+    validate(tests_runs_id=tests_runs_id, http_error_code=404)
+    tests_run = get_table('tests_runs', tests_runs_id=tests_runs_id)
     return tests_run
 
   @staticmethod
@@ -39,4 +40,4 @@ class TestsrunsID(Resource):
     api.add_resource(TestsrunsID, path)
     # directly add sub-resources of testruns 'TestrunsID'
     TestsrunsLog.add_all_resources(api, '{}/log'.format(path))
-    TestsrunsStatus.add_all_resources(api, '{}/status'.format(path))
+    TestsrunsUpdate.add_all_resources(api, '{}/update'.format(path))
